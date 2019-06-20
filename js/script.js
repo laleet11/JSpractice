47e371b8ba332d651eda61ad68cddf0b329aecd8
lalit9211 <lrj0496@gmail.com>
2019-06-20 14:25:09 +0530

    commented all validation code

---
+++ js/script.js
@@ -0,0 +1,68 @@
+// /* form -validation js */
+// function GEEKFORGEEKS()                                    
+// { 
+//     var fname = document.forms["RegForm"]["Fname"];
+//     var lname = document.forms["RegForm"]["Lname"];     
+//     var what =  document.forms["RegForm"]["Country"];
+//     var whatage =  document.forms["RegForm"]["Aged"];
+//     var email = document.forms["RegForm"]["EMail"];  
+//     var password = document.forms["RegForm"]["Password"];  
+//     var address = document.forms["RegForm"]["Paddress"];  
+
+//     if (fname.value == "")                                  
+//     { 
+//         window.alert("Please enter your name."); 
+//         name.focus(); 
+//         return false; 
+//     } 
+//     if (lname.value == "")                                  
+//     { 
+//         window.alert("Please enter your name."); 
+//         name.focus(); 
+//         return false; 
+//     }
+//     if (what.selectedIndex < 1)                  
+//     { 
+//         alert("Please select your country."); 
+//         what.focus(); 
+//         return false; 
+//     } 
+//     if (whatage.selectedIndex < 1)                  
+//     { 
+//         alert("Please select your agegroup."); 
+//         what.focus(); 
+//         return false; 
+//     }   
+//     if (email.value == "")                                   
+//     { 
+//         window.alert("Please enter a valid e-mail address."); 
+//         email.focus(); 
+//         return false; 
+//     }
+//     if (email.value.indexOf("@", 0) < 0)                 
+//     { 
+//         window.alert("Please enter a valid e-mail address."); 
+//         email.focus(); 
+//         return false; 
+//     } 
+//     if (email.value.indexOf(".", 0) < 0)                 
+//     { 
+//         window.alert("Please enter a valid e-mail address."); 
+//         email.focus(); 
+//         return false; 
+//     }  
+//    	if (password.value == "")                        
+//     { 
+//         window.alert("Please enter your password"); 
+//         password.focus(); 
+//         return false; 
+//     } 
+   
+//     if (address.value == "")                               
+//     { 
+//         window.alert("Please enter your Permanent address."); 
+//         name.focus(); 
+//         return false; 
+//     } 
+//     return true; 
+// }
